23c88b60f1223b2dfb2927e37bf626448849e53c
borrmann <max.borrmann@gmx.net>
2024-03-12 13:06:51 +0000

    Add project files.

---
+++ AndroidPermission/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+
+namespace AndroidPermission
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            try
+            {
+                PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.ContactsRead>();
+
+            }
+            catch(Exception ex)
+            {
+                await AppShell.Current.DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+
+}
